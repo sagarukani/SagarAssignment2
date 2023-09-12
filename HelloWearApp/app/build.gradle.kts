0466a28d55aed1c400a75337d441a046200dd286
sagarukani <60740300+sagarukani@users.noreply.github.com>
2023-09-12 15:27:58 -0400

    By sagar : Added launching activity

--- HelloWearApp/app/build.gradle.kts
+++ HelloWearApp/app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
 
     defaultConfig {
         applicationId = "com.example.hellowearapp"
-        minSdkPreview = "UpsideDownCakePrivacySandbox"
-        targetSdkPreview = "UpsideDownCakePrivacySandbox"
+        minSdkPreview = "30"
+        targetSdkPreview = "30"
         versionCode = 1
         versionName = "1.0"
 
@@ -36,4 +36,7 @@ dependencies {
     implementation("androidx.percentlayout:percentlayout:1.0.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.recyclerview:recyclerview:1.3.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
