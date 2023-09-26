80586b7b7b1ef91e8b593901e3b04ccc9e057c91
sagarukani <60740300+sagarukani@users.noreply.github.com>
2023-09-26 14:24:38 -0400

    By sagar ukani:
    Initial commit(project setup)

--- HelloWearApp/app/src/main/java/com/example/hellowearapp/MainActivity.java
+++ HelloWearApp/app/src/main/java/com/example/hellowearapp/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.hellowearapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvTime = findViewById(R.id.tvTime);
+
+        tvTime.setText(String.format("Today's Date is%s", new SimpleDateFormat("EEEE, MMMM d - yyyy", Locale.getDefault()).format(Calendar.getInstance().getTime())));
+
     }
 }
